92a62bb75e8455df12a2ef1c2bfdcdc2e6046d87
Rodrigo Casanova <rodrigocralha@gmail.com>
2024-02-09 01:56:12 +0000

    adding button to create new chat

--- FrontEnd/src/Services/ChatService.tsx
+++ FrontEnd/src/Services/ChatService.tsx
@@ -2,13 +2,26 @@ import axios from "axios";
 import { handleError } from "../helpers/ErrorHandler";
 import { api } from "./AuthService";
 
-type Chat = {};
+export type ChatProp = {};
 
 export const getMyChats = async (userId: string) => {
   try {
-    const data = await axios.get<Chat[]>(
+    const data = await axios.get<ChatProp[]>(
       `${api}/chats?FilterValue=${userId}&FilterBy=UserId`
     );
+    return data;
+  } catch (e: any) {
+    const { request } = handleError(e);
+    return request;
+  }
+};
+
+export const createChat = async (chatName: string, chatLogo: string) => {
+  try {
+    const data = await axios.post<ChatProp>(`${api}/group`, {
+      name: chatName,
+      logo: chatLogo,
+    });
     return data;
   } catch (e: any) {
     const { request } = handleError(e);
--- FrontEnd/src/components/ChatList.tsx
+++ FrontEnd/src/components/ChatList.tsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
-import { getMyChats } from "../Services/ChatService";
+import { FormHTMLAttributes, useEffect, useState } from "react";
+import { createChat, getMyChats } from "../Services/ChatService";
 import { useAuth } from "../Context/useAuth";
+import { ChatProp } from "../Services/ChatService";
+import { api } from "../Services/AuthService";
+import { useNavigate } from "react-router";
+import { UseFormHandleSubmit } from "react-hook-form";
+import { toast } from "react-toastify";
 
 export default function ChatList() {
   const { user } = useAuth();
-  const [chats, setChats] = useState([]);
+  const navigate = useNavigate();
+  const [chats, setChats] = useState<ChatProp[]>([]);
   const [isNewChatModalOpen, setIsNewChatModalOpen] = useState(false);
 
   useEffect(() => {
@@ -13,17 +19,52 @@ export default function ChatList() {
     });
   }, []);
 
+  const createChatModule = async (formData: FormData) => {
+    const name = formData.get("name")?.toString();
+    const logo = formData.get("logo")?.toString();
+    if (!name || !logo) return toast.error("Name and logo are required");
+
+    const data = await createChat(name, logo);
+    console.log({ data });
+    setIsNewChatModalOpen(false);
+  };
+
   return (
-    <div className="col-span-2 h-full bg-zinc-800 text-white">
-      <h1>ChatList</h1>
+    <div className="p-1 col-span-2 h-full bg-zinc-800 text-white">
       <button onClick={() => setIsNewChatModalOpen(!isNewChatModalOpen)}>
         New Chat
       </button>
       {isNewChatModalOpen && (
-        <div>
-          <input type="text" placeholder="Chat name" />
-          <button>Create</button>
-        </div>
+        <form
+          method={`${api}/api/group`}
+          action="POST"
+          className="flex flex-col gap-2 py-1"
+          onSubmit={(e) => {
+            e.preventDefault();
+            createChatModule(new FormData(e.target as HTMLFormElement));
+          }}
+        >
+          <input
+            className="p-1 text-sm text-black outline-none"
+            type="text"
+            placeholder="Chat name"
+            name="name"
+            required
+          />
+          <input
+            type="text"
+            className="p-1 text-sm text-black outline-none"
+            placeholder="Image url"
+            name="logo"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-green-500 font-medium hover:bg-green-600"
+          >
+            Create
+          </button>
+        </form>
       )}
       <div>
         {chats.length == 0 ? (
